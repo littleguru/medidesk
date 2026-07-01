236ac9b7c797a61b8f40e7538298a459c6efecfb
Chris <chriskamaumbugua@gmail.com>
2026-07-01 15:48:08 +0300

    Hardcode ALGORITHM default

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -12,7 +12,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM")
+ALGORITHM = os.getenv("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
